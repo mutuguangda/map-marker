ea3de73616be470fafda2462827a14e3140f8d29
mutuguangda <mutuguangda@foxmail.com>
2024-04-03 14:07:53 +0800

    fix: typo

--- components/chart/bmap.tsx
+++ components/chart/bmap.tsx
@@ -1,9 +1,11 @@
 "use client"
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import AMapLoader from "@amap/amap-jsapi-loader";
+import '@amap/amap-jsapi-types';
 
 export default function MapContainer() {
-  let map = null;
+  const el = useRef<HTMLDivElement | null>(null) 
+  const map = useRef<AMap.Map | null>(null);
 
   useEffect(() => {
     AMapLoader.load({
@@ -11,27 +13,29 @@ export default function MapContainer() {
       version: "2.0", // 指定要加载的 JSAPI 的版本，缺省时默认为 1.4.15
       plugins: [], // 需要使用的的插件列表，如比例尺'AMap.Scale'等
     })
-      .then((AMap) => {
-        map = new AMap.Map("container", {
+      .then((_AMap: typeof AMap) => {
+        map.current = new _AMap.Map(el.current!, {
           // 设置地图容器id
           viewMode: "3D", // 是否为3D地图模式
           zoom: 11, // 初始化地图级别
           center: [116.397428, 39.90923], // 初始化地图中心点位置
         });
+        const mapStyle = 'amap://styles/grey'; // 使用灰色风格
+        map.current?.setMapStyle(mapStyle);
       })
       .catch((e) => {
         console.log(e);
       });
 
     return () => {
-      map?.destroy();
+      map.current?.destroy();
     };
   }, []);
 
   return (
     <div
-      id="container"
       className="main"
+      ref={el}
     ></div>
   );
 }
