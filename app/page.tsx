ff558d1b630e95c51a8d45207e45dd8475082a77
mutuguangda <mutuguangda@foxmail.com>
2024-04-05 01:44:22 +0800

    pref: [WIP] 优化 _copyToClipboard 和 toast 触发的视图更新

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { KeyboardEventHandler, MouseEventHandler, useEffect, useRef, useState } from "react";
+import { KeyboardEventHandler, MouseEventHandler, useCallback, useEffect, useRef, useState } from "react";
 // import * as echarts from "echarts";
 import { InputSearch } from "@/components/ui/input-search"
 import { useCopyToClipboard } from "react-use";
@@ -23,74 +23,35 @@ export default function Home() {
   const [positions, setPositions] = useState(data.geocodes)
   const [currentOption, setCurrentOption] = useState(baseOption.current)
 
-  const [_, copyToClipboard] = useCopyToClipboard();
-
+  // 会触发视图更新
+  const [_, _copyToClipboard] = useCopyToClipboard();
+  const copyToClipboard = useCallback((s: string) => {
+    _copyToClipboard(s)
+  }, [_copyToClipboard]);
   const { toast } = useToast()
 
   useEffect(() => {
     const down = (e: KeyboardEvent) => {
       if (e.key === "s" && (e.metaKey || e.ctrlKey)) {
         e.preventDefault()
-        handleExport()
+        const hash = utoa(JSON.stringify(currentOption))
+        const url = `${window.location.origin}/m/#${hash}`
+        copyToClipboard(url)
+        toast({
+          title: '已导出到剪贴板',
+          description: (
+            <div className="whitespace-preline truncate">
+              <a href={url} target="_blank">{url}</a>
+            </div>
+          )
+        })
       }
     }
     document.addEventListener("keydown", down)
     return () => document.removeEventListener("keydown", down)
   })
 
-  const handleKeyDown: KeyboardEventHandler<HTMLInputElement> = (e) => {
-    if (e.code !== 'Enter') {
-      return
-    }
-
-    const input = (e.target as unknown as any).value
-    if (!input) {
-      return
-    }
-    fetch(`https://restapi.amap.com/v3/geocode/geo?address=${input}&output=JSON&key=9e916cb1a5436f165a8317d249c99e39	`).then(res => res.json()).then(data => {
-      setPositions(data.geocodes)
-    })
-  }
-
-  const handleClick: MouseEventHandler<HTMLDivElement> = (e) => {
-    const position = positions[(e.target as unknown as any).getAttribute('data-index')]
-    const point: PointType = { 
-      title: position['formatted_address'], 
-      position: position.location.split(',').map(parseFloat) as unknown as any,
-      description: ''
-    }
-    setCurrentOption(() => {
-      currentOption.points[position.location] = point
-      return cloneDeep(currentOption)
-    })
-  }
-
-  const handleExport = () => {
-    const hash = utoa(JSON.stringify(currentOption))
-    const url = `${window.location.origin}/m/#${hash}`
-    copyToClipboard(url)
-    toast({
-      title: '已导出到剪贴板',
-      description: (
-        <div className="whitespace-preline">
-          <a href={url} target="_blank">{url}</a>
-        </div>
-      )
-    })
-  }
-
   return (
-    <div className="main relative">
-      <div className="bg-background w-96 p-3 absolute left-5 top-5 z-50 border rounded-md">
-        <Button onClick={handleExport}>导出</Button>
-        <InputSearch onKeyDown={handleKeyDown} />
-        <div className="mt-2 flex flex-col gap-2" onClick={handleClick}>
-          {positions.map((position, index) => {
-            return <div className="p-2 border" key={position.location} data-index={index}>{position.formatted_address}</div>
-          })}
-        </div>
-      </div>
-      <MapContainer className="w-full h-full" option={currentOption} />
-    </div>
+    <MapContainer className="main" option={currentOption} />
   );
 }
--- components/chart/amap.tsx
+++ components/chart/amap.tsx
@@ -1,9 +1,18 @@
 "use client"
-import { useEffect, useRef } from "react";
+import { KeyboardEventHandler, MouseEventHandler, useEffect, useRef, useState } from "react";
 import { createRoot } from 'react-dom/client';
 import AMapLoader from "@amap/amap-jsapi-loader";
 import '@amap/amap-jsapi-types';
 import { Input } from "../ui/input";
+import { InputSearch } from "../ui/input-search";
+import { Button } from "../ui/button";
+import { ScrollArea } from '../ui/scroll-area'
+import { cloneDeep } from "lodash-es";
+import { useCopyToClipboard } from "react-use";
+import { useToast } from "../ui/use-toast";
+import data from './data.json' 
+import { utoa } from "@/app/utils";
+import { Textarea } from "../ui/textarea";
 
 interface PropsType {
   className?: string
@@ -27,9 +36,18 @@ export default function MapContainer({ className, option }: PropsType) {
   const el = useRef<HTMLDivElement | null>(null) 
   const map = useRef<AMap.Map | null>(null);
   const infoWindow = useRef<AMap.InfoWindow | null>(null)
+  const inputSearch = useRef<HTMLInputElement | null>(null)
+  const scrollContainer = useRef<HTMLDivElement | null>(null)
+
   const mapStyle = option.mapStyle || 'amap://styles/a927524f8e540e512b9bdea1cad3d6fb'; // 使用灰色风格
 
   const GMap = useRef<typeof AMap | null>(null) 
+
+  const [positions, setPositions] = useState(data.geocodes)
+
+  const [_, copyToClipboard] = useCopyToClipboard();
+
+  const { toast } = useToast()
 
   useEffect(() => {
     // @ts-ignore
@@ -37,7 +55,7 @@ export default function MapContainer({ className, option }: PropsType) {
     AMapLoader.load({
       key: "dafe1244843ac4a6721a014970c63558",
       version: "2.0",
-      plugins: [],
+      plugins: ["AMap.AutoComplete"], //需要使用的的插件列表
     })
       .then((_AMap: typeof AMap) => {
         GMap.current = _AMap
@@ -54,6 +72,21 @@ export default function MapContainer({ className, option }: PropsType) {
         Object.keys(points).forEach((key) => {
           setPoint(points[key])
         })
+
+        // @ts-ignore
+        const autoComplete = new _AMap.AutoComplete({
+          input: inputSearch.current,
+          output: scrollContainer.current
+        })
+        autoComplete.on('select', (e: any) => {
+          const { name, location } = e.poi
+          const key = location.pos.join()
+          if (points[key]) return
+          const point = { title: name, position: location, description: '' }
+          points[key] = point
+          setPoint({ title: name, position: location, description: '' })
+        })
+        map.current.addControl(autoComplete)
       })
       .catch((e) => {
         console.log(e);
@@ -63,6 +96,17 @@ export default function MapContainer({ className, option }: PropsType) {
       map.current?.destroy();
     };
   });
+
+  useEffect(() => {
+    const down = (e: KeyboardEvent) => {
+      if (e.key === "q" && (e.metaKey || e.ctrlKey)) {
+        e.preventDefault()
+        inputSearch.current?.focus()
+      }
+    }
+    document.addEventListener("keydown", down)
+    return () => document.removeEventListener("keydown", down)
+  })
 
   function setPoint(point: BMapOptionType['points'][string]) {
     const marker = new GMap.current!.Marker({
@@ -74,8 +118,8 @@ export default function MapContainer({ className, option }: PropsType) {
     marker.content = element
     const elementChildren = (
       <div className="flex flex-col gap-2">
-        <h3>{point.title}</h3>
-        <Input 
+        <h6 className="text-xs">{point.title}</h6>
+        <Textarea
           defaultValue={point.description}
           onChange={(e) => {
             point.description = e.target.value
@@ -87,15 +131,27 @@ export default function MapContainer({ className, option }: PropsType) {
     root.render(elementChildren)
     marker.on('click', (e) => {
       infoWindow.current?.setContent(e.target.content)
-      infoWindow.current?.open(map.current!, e.target.getPosition());
+      const position = e.target.getPosition()
+      setTimeout(() => {
+        infoWindow.current?.open(map.current!, position);
+      }, 50)
     })
     marker.emit('click', { target: marker })
   }
 
   return (
-    <div
-      ref={el}
-      className={className}
-    />
+    <div className="w-full h-full relative">
+      <div className="bg-background w-96 p-3 absolute left-5 top-5 z-50 border rounded-md">
+        {/* <Button onClick={handleExport}>导出</Button> */}
+        <InputSearch ref={inputSearch} />
+        <ScrollArea className="mt-2">
+          <div ref={scrollContainer}></div>
+        </ScrollArea>
+      </div>
+      <div
+        ref={el}
+        className={className}
+      />
+    </div>
   );
 }
