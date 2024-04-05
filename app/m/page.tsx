836fa783a4506a154490b1be0c7c7e90f065b3c8
mutuguangda <mutuguangda@foxmail.com>
2024-04-05 18:21:24 +0800

    fix: typo

--- app/m/page.tsx
+++ app/m/page.tsx
@@ -7,7 +7,7 @@ import { useRouter } from 'next/router'
 type Props = {}
 
 export default function Page({}: Props) {
-  const hash = typeof window !== undefined ? window.location.hash: ''
+  const hash = typeof window !== 'undefined' ? window.location.hash: ''
 
   let baseOption
   try {
